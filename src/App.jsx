c78e0700611922baecfb17c71190141b8feafdb2
Luigi Gandemer <luigigandemer6@gmail.com>
2026-02-10 14:39:42 +0100

    Lifting State Up (Ancestor / Child components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
+import {useState} from "react";
+
 import {Player} from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
-  
+    const [activePlayer, setActivePlayer] = useState( 'X');
 
-  return (
-      <main>
-        <div id='game-container'>
-          <ol id='players'>
-            <Player initialName="Player 1" symbol="X" />
-            <Player initialName="Player 2" symbol="O" />
-          </ol>
+    function handleSelectSquare() {
+        setActivePlayer((currentPlayer) => currentPlayer === 'X' ? 'O' : 'X');
+    }
 
-          <GameBoard />
-        </div>
-
-        LOG
-      </main>
-  );
+    return (
+        <main>
+            <div id='game-container'>
+                <ol id='players' className='highlight-player'>
+                    <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+                    <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+                </ol>
+                <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+            </div>
+            LOG
+        </main>
+    );
 }
 
 export default App
