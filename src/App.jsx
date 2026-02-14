754b6e0aaa9f877063630c5dffc7366ce484ee01
Luigi Gandemer <luigigandemer6@gmail.com>
2026-02-14 14:51:54 +0100

    Reducing state management

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import {Player} from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 
+function deriveActivePlauer(gameTurns) {
+    let currentPlayer = 'X';
+    if(gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+    }
+
+    return currentPlayer;
+}
+
 function App() {
-    const [activePlayer, setActivePlayer] = useState( 'X');
     const [gameTurns, setGameTurns] = useState( []);
+    const activePlayer = deriveActivePlauer(gameTurns);
 
     function handleSelectSquare(rowIndex, colIndex) {
-        setActivePlayer((currentPlayer) => currentPlayer === 'X' ? 'O' : 'X');
-
         setGameTurns(prevTurns => {
-            let currentPlayer = 'X';
-
-            if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
-                currentPlayer = 'O';
-            }
+            const currentPlayer = deriveActivePlauer(prevTurns);
 
             const updatedTurns = [
                 {
