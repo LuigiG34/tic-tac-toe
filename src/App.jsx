875ed7f3a425b03ebad98f261b193fc6709c7eca
Luigi Gandemer <luigigandemer6@gmail.com>
2026-02-14 15:18:23 +0100

    Game Over screen + Checking draw

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Player} from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+import GameOver from "./components/GameOver.jsx";
 
 const initialGameBoard = [
     [null, null, null],
@@ -50,6 +51,8 @@ function App() {
         }
     }
 
+    const hasDraw = gameTurns.length === 9 && !winner;
+
     function handleSelectSquare(rowIndex, colIndex) {
         setGameTurns(prevTurns => {
             const currentPlayer = deriveActivePlayer(prevTurns);
@@ -73,7 +76,7 @@ function App() {
                     <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
                     <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
                 </ol>
-                {winner && <p>You won, {winner} !</p>}
+                {(winner || hasDraw) && <GameOver winner={winner} />}
                 <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} board={gameBoard} />
             </div>
             <Log turns={gameTurns} />
